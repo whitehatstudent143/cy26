334a3ad87e4bbe403be3628114530af0c598218d
whitehatstudent143@gmail.com <whitehatstudent143@gmail.com>
2020-11-19 07:51:36 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -34,6 +34,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    platform= new Ground(100,300,200,200);
 
 }
 
@@ -57,6 +58,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    platform.display();
 
     bird.display();
 }
